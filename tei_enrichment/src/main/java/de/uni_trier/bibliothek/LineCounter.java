1fdaa9f5ade8685f5d61d3772b205f5c654bac19
René Ackels <ackels@uni-trier.de>
2023-10-30 16:24:32 +0100

    linecounter wip

--- tei_enrichment/src/main/java/de/uni_trier/bibliothek/LineCounter.java
+++ tei_enrichment/src/main/java/de/uni_trier/bibliothek/LineCounter.java
@@ -18,30 +18,162 @@
 
 package de.uni_trier.bibliothek;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.uni_trier.bibliothek.xml.tei.model.generated.Choice;
+import de.uni_trier.bibliothek.xml.tei.model.generated.DivFront;
+import de.uni_trier.bibliothek.xml.tei.model.generated.DocTitle;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Front;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Lb;
+import de.uni_trier.bibliothek.xml.tei.model.generated.LbEtc;
+import de.uni_trier.bibliothek.xml.tei.model.generated.ObjectFactory;
+import de.uni_trier.bibliothek.xml.tei.model.generated.PFront;
+import de.uni_trier.bibliothek.xml.tei.model.generated.Pb;
 import de.uni_trier.bibliothek.xml.tei.model.generated.TEI;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Text;
+import de.uni_trier.bibliothek.xml.tei.model.generated.TitlePage;
 import jakarta.xml.bind.JAXBElement;
 
 public class LineCounter {
 
+	public static int lineNumber;
 	public static TEI originalTEI;
-	public static de.uni_trier.bibliothek.xml.tei.model.generated.ObjectFactory personsTEIObjectFactory = new de.uni_trier.bibliothek.xml.tei.model.generated.ObjectFactory();
+	public static ObjectFactory personsTEIObjectFactory = new ObjectFactory();
 
-	public static TEI countLines(TEI originalTEIParameter)
-	{
-		originalTEI = originalTEIParameter;		
-		
+	public static TEI countLines(TEI originalTEIParameter) {
+		lineNumber = 1;
+		originalTEI = originalTEIParameter;
+		System.out.println("start counting Lines");
 		Text originText = originalTEI.getText();
-		Front front = originText.getFront();
-		List<Object> pbOrDivOrTitlePage = front.getPbOrDivOrTitlePage();		
-
+		checkText(originText);
 		return originalTEI;
 	}
 
+	public static void checkText(Text text) {
+		Front front = text.getFront();
+		checkFront(front);
+	}
+
+	public static void checkFront(Front front) {
+		List<Object> pbOrDivOrTitlePageList = front.getPbOrDivOrTitlePage();
+		// System.out.println("liste von front: " + pbOrDivOrTitlePageList.toString());
+		for (Object pbOrDivOrTitlePage : pbOrDivOrTitlePageList) {
+			if (pbOrDivOrTitlePage instanceof DivFront) {
+				DivFront divFrontElement = (DivFront) pbOrDivOrTitlePage;
+				checkDivFront(divFrontElement);
+			}
+			if (pbOrDivOrTitlePage instanceof TitlePage) {
+				TitlePage titlePage = (TitlePage) pbOrDivOrTitlePage;
+				checkTitlePage(titlePage);
+			}
+
+		}
+	}
+
+	public static void checkDivFront(DivFront divFrontElement) {
+		List<JAXBElement<?>> divFrontList = divFrontElement.getFwOrPOrFigure();
+		for (JAXBElement<?> divFrontListElement : divFrontList) {
+			if (divFrontListElement.getValue() instanceof PFront) {
+				PFront pElement = (PFront) divFrontListElement.getValue();
+				checkPFront(pElement);
+				// System.out.println("pElements: " + pElement.getContent());
+				
+			}
+		}
+	}
+
+	public static void checkTitlePage(TitlePage titlePage) {
+		List<Serializable> titlePageList = titlePage.getContent();
+		for (Object titlePageListElement : titlePageList) {
+			if (!(titlePageListElement instanceof String))
+			{
+				JAXBElement<?> titlePageListElementObject = (JAXBElement<?>) titlePageListElement;
+
+				if (titlePageListElementObject.getValue() instanceof DocTitle) {
+				DocTitle docTitle = (DocTitle) titlePageListElementObject.getValue();
+				checkDocTitle(docTitle);
+				// System.out.println("pElements: " + pElement.getContent());
+				
+			}
+
+			}
+			
+		}
+	}
+
+	public static void checkDocTitle(DocTitle docTitle) {
+		//todo
+	}
+	
+
+	public static void checkPFront(PFront pfront) {
+		List<Serializable> pList = pfront.getContent();
+				// System.out.println("pList: " + pList);
+				for (Object pListElement : pList) {
+					// JAXBElement<?> jaxbElement;
+					if (!(pListElement instanceof String)) {
+						JAXBElement<?> jaxbElement = (JAXBElement<?>) pListElement;
+						if(jaxbElement.getValue() instanceof Lb)
+						{
+							Lb LbElement = (Lb) jaxbElement.getValue();
+							setNinLb(LbElement);
+						}
+						else if(jaxbElement.getValue() instanceof Choice)
+						{
+							Choice choiceElement = (Choice) jaxbElement.getValue();
+							checkChoice(choiceElement);
+						}
+					}
+				}
+	}
+
+	public static void checkChoice(Choice choice) {
+			List<JAXBElement<?>> choiceElementsList = choice.getAbbrOrExpanOrOrig();
+
+			for (JAXBElement<?> abbrOrExpanOrOrigElement : choiceElementsList) {
+				if (abbrOrExpanOrOrigElement.getValue() instanceof LbEtc) {
+					LbEtc lbEtc = (LbEtc)abbrOrExpanOrOrigElement.getValue();
+					checkLbEtc(lbEtc);
+				}
+
+			}
+		
+
+	}
+
+	public static void checkLbEtc(LbEtc lbEtc) {
+
+		List<Serializable> lbEtcList = lbEtc.getContent();
+
+		for (Object lbEtcListElement : lbEtcList) {
+			if (!(lbEtcListElement instanceof String)) {
+				JAXBElement<?> jaxbLbEtcListElement = (JAXBElement<?>) lbEtcListElement;
+				
+				if (jaxbLbEtcListElement.getValue() instanceof Choice) {
+					Choice choice = (Choice)jaxbLbEtcListElement.getValue();
+					checkChoice(choice);
+
+				}
+			 else if (lbEtcListElement instanceof Lb) {
+				
+				Lb LbElement = (Lb) jaxbLbEtcListElement.getValue();
+					setNinLb(LbElement);
+				
+			}
+		}
+		}
+	}
+
+	
+
+	public static void setNinLb(Lb lbElement) {
+
+			System.out.println("lb ist: " + lbElement.getN());
+			lbElement.setN(Integer.toString(lineNumber));
+			lineNumber++;
+
+	}
 
 }
