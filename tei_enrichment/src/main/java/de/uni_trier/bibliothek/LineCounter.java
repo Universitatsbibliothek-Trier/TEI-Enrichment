078c7abe3324b6d4f2e7b45f48ff63f3f7eac841
René Ackels <ackels@uni-trier.de>
2023-11-08 17:01:34 +0100

    linecounter work in progress

--- tei_enrichment/src/main/java/de/uni_trier/bibliothek/LineCounter.java
+++ tei_enrichment/src/main/java/de/uni_trier/bibliothek/LineCounter.java
@@ -21,6 +21,7 @@ package de.uni_trier.bibliothek;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Attributes.Name;
 
 import de.uni_trier.bibliothek.xml.tei.model.generated.Back;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Body;
@@ -35,6 +36,7 @@ import de.uni_trier.bibliothek.xml.tei.model.generated.GroupBody;
 import de.uni_trier.bibliothek.xml.tei.model.generated.GroupText;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Head;
 import de.uni_trier.bibliothek.xml.tei.model.generated.InnerGroup;
+import de.uni_trier.bibliothek.xml.tei.model.generated.Item;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Lb;
 import de.uni_trier.bibliothek.xml.tei.model.generated.LbEtc;
 import de.uni_trier.bibliothek.xml.tei.model.generated.NameGND;
@@ -44,6 +46,7 @@ import de.uni_trier.bibliothek.xml.tei.model.generated.OuterGroup;
 import de.uni_trier.bibliothek.xml.tei.model.generated.PFront;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Pb;
 import de.uni_trier.bibliothek.xml.tei.model.generated.PbFront;
+import de.uni_trier.bibliothek.xml.tei.model.generated.Row;
 import de.uni_trier.bibliothek.xml.tei.model.generated.SourceGND;
 import de.uni_trier.bibliothek.xml.tei.model.generated.TEI;
 import de.uni_trier.bibliothek.xml.tei.model.generated.Table;
@@ -127,16 +130,13 @@ public class LineCounter {
 			}
 			else if (divOrPbOrLbListElement.getValue() instanceof SourceGND) {
 				SourceGND sourceElement = (SourceGND) divOrPbOrLbListElement.getValue();
-				checkSource(sourceElement);
+				checkSourceGND(sourceElement);
 			}
-			else if (divOrPbOrLbListElement.getValue() instanceof Gap) {
-				Gap gapElement = (Gap) divOrPbOrLbListElement.getValue();
-				checkGap(gapElement);
-			}
-			else if (divOrPbOrLbListElement.getValue() instanceof Num) {
-				Num numElement = (Num) divOrPbOrLbListElement.getValue();
-				checkNum(numElement);
-			}
+			// else if (dS
+			// else if (divOrPbOrLbListElement.getValue() instanceof Num) {
+			// 	Num numElement = (Num) divOrPbOrLbListElement.getValue();
+			// 	checkNum(numElement);
+			// }
 			else if (divOrPbOrLbListElement.getValue() instanceof LbEtc) {
 				LbEtc lbEtcElement = (LbEtc) divOrPbOrLbListElement.getValue();
 				checkLbEtc(lbEtcElement);
@@ -204,16 +204,16 @@ public class LineCounter {
 			}
 			else if (divOrPbOrLbListElement.getValue() instanceof SourceGND) {
 				SourceGND sourceElement = (SourceGND) divOrPbOrLbListElement.getValue();
-				checkSource(sourceElement);
+				checkSourceGND(sourceElement);
 			}
-			else if (divOrPbOrLbListElement.getValue() instanceof Gap) {
-				Gap gapElement = (Gap) divOrPbOrLbListElement.getValue();
-				checkGap(gapElement);
-			}
-			else if (divOrPbOrLbListElement.getValue() instanceof Num) {
-				Num numElement = (Num) divOrPbOrLbListElement.getValue();
-				checkNum(numElement);
-			}
+			// else if (divOrPbOrLbListElement.getValue() instanceof Gap) {
+			// 	Gap gapElement = (Gap) divOrPbOrLbListElement.getValue();
+			// 	checkGap(gapElement);
+			// }
+			// else if (divOrPbOrLbListElement.getValue() instanceof Num) {
+			// 	Num numElement = (Num) divOrPbOrLbListElement.getValue();
+			// 	checkNum(numElement);
+			// }
 			else if (divOrPbOrLbListElement.getValue() instanceof LbEtc) {
 				LbEtc lbEtcElement = (LbEtc) divOrPbOrLbListElement.getValue();
 				checkLbEtc(lbEtcElement);
@@ -256,7 +256,106 @@ public class LineCounter {
 				Lb lbElement = (Lb) divOrPbOrLbElement;
 				setNinLb(lbElement);
 			}
+			else if (divOrPbOrLbElement instanceof Fw) {
+				Fw fwElement = (Fw) divOrPbOrLbElement;
+				checkFw(fwElement);
+			}
 
+
+		}
+	}
+
+	public static void checkList(de.uni_trier.bibliothek.xml.tei.model.generated.List list) {
+		List<Serializable> listElement = list.getContent();
+		for (Object listElementObject : listElement) {
+			if (!(listElementObject instanceof String)) {
+			
+			
+			JAXBElement<?> jaxbElement = (JAXBElement<?>) listElementObject;
+			if (jaxbElement.getValue() instanceof Lb) {
+				Lb lbElement = (Lb) jaxbElement.getValue();
+				setNinLb(lbElement);
+			}
+			else if (jaxbElement.getValue() instanceof Item) {
+				Item itemElement = (Item) jaxbElement.getValue();
+				checkItem(itemElement);
+			}
+			else if (jaxbElement.getValue() instanceof Fw) {
+				Fw fwElement = (Fw) jaxbElement.getValue();
+				checkFw(fwElement);
+			}	
+			}
+
+
+		}
+	}
+
+	public static void checkItem(Item item) {
+		List<Serializable> itemElement = item.getContent();
+		for (Object itemElementObject : itemElement) {
+			if (!(itemElementObject instanceof String)) {
+			
+			
+			JAXBElement<?> jaxbElement = (JAXBElement<?>) itemElementObject;
+			if (jaxbElement.getValue() instanceof Lb) {
+				Lb lbElement = (Lb) jaxbElement.getValue();
+				setNinLb(lbElement);
+			}
+			else if (jaxbElement.getValue() instanceof Choice) {
+				Choice choiceElement = (Choice) jaxbElement.getValue();
+				checkChoice(choiceElement);
+			}
+			else if (jaxbElement.getValue() instanceof de.uni_trier.bibliothek.xml.tei.model.generated.List) {
+				de.uni_trier.bibliothek.xml.tei.model.generated.List listElement = (de.uni_trier.bibliothek.xml.tei.model.generated.List) jaxbElement.getValue();
+				checkList(listElement);
+			}	
+
+
+			
+			}
+
+
+		}
+	}
+
+	public static void checkTable(Table table) {
+		List<Serializable> tableList = table.getContent();
+		for (Object tableElement : tableList) {
+			if (!(tableElement instanceof String)) {
+				JAXBElement<?> jaxbElement = (JAXBElement<?>) tableElement;
+	
+			// System.out.println("liste von front: " + pbOrDivOrTitlePageList.toString());
+	
+			if (jaxbElement.getValue() instanceof Lb) {
+				Lb lbElement = (Lb) jaxbElement.getValue();
+				setNinLb(lbElement);
+			}
+			else if (jaxbElement.getValue() instanceof Row) {
+				Row rowElement = (Row) jaxbElement.getValue();
+				checkRow(rowElement);
+			}
+		}
+		}
+	}
+
+
+	public static void checkRow(Row row) {
+		List<Serializable> rowList = row.getContent();
+		for (Object rowElement : rowList) {
+			if (!(rowElement instanceof String)) {
+				JAXBElement<?> jaxbElement = (JAXBElement<?>) rowElement;
+	
+			// System.out.println("liste von front: " + pbOrDivOrTitlePageList.toString());
+	
+			if (jaxbElement.getValue() instanceof Lb) {
+				Lb lbElement = (Lb) jaxbElement.getValue();
+				setNinLb(lbElement);
+			}
+			else if (jaxbElement.getValue() instanceof LbEtc) {
+				LbEtc lbEtcElement = (LbEtc) jaxbElement.getValue();
+				checkLbEtc(lbEtcElement);
+			}
+		}
 		}
 	}
 
@@ -312,6 +411,36 @@ public class LineCounter {
 		}
 	}
 
+	public static void checkHead(Head head) {
+		List<Serializable> headList = head.getContent();
+		// System.out.println("liste von front: " + pbOrDivOrTitlePageList.toString());
+		for (Object headListElement : headList) {
+			
+			if (!(headListElement instanceof String))
+			{
+				JAXBElement<?> jaxbHeadListElement = (JAXBElement<?>) headListElement;
+				if (jaxbHeadListElement.getValue() instanceof Lb) {
+					Lb lbElement = (Lb) jaxbHeadListElement.getValue();
+					setNinLb(lbElement);
+				}
+				else if (jaxbHeadListElement.getValue() instanceof NameGND) {
+					NameGND nameGNDElement = (NameGND) jaxbHeadListElement.getValue();
+					checkNameGnd(nameGNDElement);
+				}
+				else if (jaxbHeadListElement.getValue() instanceof SourceGND) {
+					SourceGND sourceGNDElement = (SourceGND) jaxbHeadListElement.getValue();
+					checkSourceGND(sourceGNDElement);
+				}
+				else if (jaxbHeadListElement.getValue() instanceof LbEtc) {
+					LbEtc lbEtcElement = (LbEtc) jaxbHeadListElement.getValue();
+					checkLbEtc(lbEtcElement);
+				}
+
+		}
+		}
+	}
+
+
 	public static void checkTitlePage(TitlePage titlePage) {
 		List<Serializable> titlePageList = titlePage.getContent();
 		for (Object titlePageListElement : titlePageList) {
@@ -343,11 +472,49 @@ public class LineCounter {
 					LbEtc lbEtc = (LbEtc) titlePageListElementObject.getValue();
 					checkLbEtc(lbEtc);
 				}
+				else if (titlePageListElementObject.getValue() instanceof Fw) {
+					Fw fwElement = (Fw) titlePageListElementObject.getValue();
+					checkFw(fwElement);
+				}
 
 
 			}
 			
 		}
+	}
+
+	public static void checkDocImprint(DocImprint docImprint)
+	{
+		List<JAXBElement<?>> docImprintList = docImprint.getPubPlaceOrPublisherOrLb();
+		for (JAXBElement<?> jaxbElement : docImprintList) {
+						if(jaxbElement.getValue() instanceof LbEtc)
+						{
+							LbEtc lbEtc = (LbEtc) jaxbElement.getValue();
+							checkLbEtc(lbEtc);
+						}
+						else if(jaxbElement.getValue() instanceof Lb)
+						{
+							Lb LbElement = (Lb) jaxbElement.getValue();
+							setNinLb(LbElement);
+						}
+						else if(jaxbElement.getValue() instanceof Choice)
+						{
+							Choice choiceElement = (Choice) jaxbElement.getValue();
+							checkChoice(choiceElement);
+						}
+						
+						else if(jaxbElement.getValue() instanceof NameGND)
+						{
+							NameGND nameGND = (NameGND) jaxbElement.getValue();
+							checkNameGnd(nameGND);
+						}
+						else if(jaxbElement.getValue() instanceof SourceGND)
+						{
+							SourceGND sourceGND = (SourceGND) jaxbElement.getValue();
+							checkSourceGND(sourceGND);
+						}
+					}
+		
 	}
 
 	public static void checkDocTitle(DocTitle docTitle) {
@@ -411,7 +578,16 @@ public class LineCounter {
 							NameGND nameGND = (NameGND) jaxbElement.getValue();
 							checkNameGnd(nameGND);
 						}
-						//todo
+						else if(jaxbElement.getValue() instanceof SourceGND)
+						{
+							SourceGND sourceGND = (SourceGND) jaxbElement.getValue();
+							checkSourceGND(sourceGND);
+						}
+						else if(jaxbElement.getValue() instanceof LbEtc)
+						{
+							LbEtc lbEtc = (LbEtc) jaxbElement.getValue();
+							checkLbEtc(lbEtc);
+						}
 					}
 
 		}
@@ -438,6 +614,52 @@ public class LineCounter {
 							NameGND nameInNameGND = (NameGND) jaxbElement.getValue();
 							checkNameGnd(nameInNameGND);
 						}
+						else if(jaxbElement.getValue() instanceof LbEtc)
+						{
+							LbEtc lbEtc = (LbEtc) jaxbElement.getValue();
+							checkLbEtc(lbEtc);
+						}
+						else if(jaxbElement.getValue() instanceof SourceGND)
+						{
+							SourceGND sourceGND = (SourceGND) jaxbElement.getValue();
+							checkSourceGND(sourceGND);
+						}
+					}
+
+		}
+
+	}
+
+	public static void checkSourceGND(SourceGND sourceGNDGND) {
+		List<Serializable> sourceGNDList = sourceGNDGND.getContent();
+		for (Object sourceGNDObject : sourceGNDList) {
+			if (!(sourceGNDObject instanceof String)) {
+						JAXBElement<?> jaxbElement = (JAXBElement<?>) sourceGNDObject;
+						if(jaxbElement.getValue() instanceof Lb)
+						{
+							Lb LbElement = (Lb) jaxbElement.getValue();
+							setNinLb(LbElement);
+						}
+						else if(jaxbElement.getValue() instanceof Choice)
+						{
+							Choice choiceElement = (Choice) jaxbElement.getValue();
+							checkChoice(choiceElement);
+						}
+						else if(jaxbElement.getValue() instanceof NameGND)
+						{
+							NameGND nameInNameGND = (NameGND) jaxbElement.getValue();
+							checkNameGnd(nameInNameGND);
+						}
+						else if(jaxbElement.getValue() instanceof LbEtc)
+						{
+							LbEtc lbEtc = (LbEtc) jaxbElement.getValue();
+							checkLbEtc(lbEtc);
+						}
+						else if(jaxbElement.getValue() instanceof SourceGND)
+						{
+							SourceGND sourceGND = (SourceGND) jaxbElement.getValue();
+							checkSourceGND(sourceGND);
+						}
 					}
 
 		}
@@ -447,9 +669,7 @@ public class LineCounter {
 
 	public static void checkPFront(PFront pfront) {
 		List<Serializable> pList = pfront.getContent();
-				// System.out.println("pList: " + pList);
 				for (Object pListElement : pList) {
-					// JAXBElement<?> jaxbElement;
 					if (!(pListElement instanceof String)) {
 						JAXBElement<?> jaxbElement = (JAXBElement<?>) pListElement;
 						if(jaxbElement.getValue() instanceof Lb)
@@ -462,6 +682,45 @@ public class LineCounter {
 							Choice choiceElement = (Choice) jaxbElement.getValue();
 							checkChoice(choiceElement);
 						}
+						else if(jaxbElement.getValue() instanceof Pb)
+						{
+							lineNumber = 1;
+						}
+						else if(jaxbElement.getValue() instanceof Fw)
+						{
+							Fw fwElement = (Fw) jaxbElement.getValue();
+							checkFw(fwElement);
+						}
+						else if(jaxbElement.getValue() instanceof NameGND)
+						{
+							NameGND nameGNDElement = (NameGND) jaxbElement.getValue();
+							checkNameGnd(nameGNDElement);
+						}
+						else if(jaxbElement.getValue() instanceof SourceGND)
+						{
+							SourceGND sourceGNDElement = (SourceGND) jaxbElement.getValue();
+							checkSourceGND(sourceGNDElement);
+						}
+						// else if(jaxbElement.getValue() instanceof Num)
+						// {
+						// 	Num numElement = (Num) jaxbElement.getValue();
+						// 	checkNum(numElement);
+						// }
+						// else if(jaxbElement.getValue() instanceof Gap)
+						// {
+						// 	Gap gapElement = (Gap) jaxbElement.getValue();
+						// 	checkGap(gapElement);
+						// }
+						else if(jaxbElement.getValue() instanceof LbEtc)
+						{
+							LbEtc lbEtcElement = (LbEtc) jaxbElement.getValue();
+							checkLbEtc(lbEtcElement);
+						}
+						else if(jaxbElement.getValue() instanceof Table)
+						{
+							Table tableElement = (Table) jaxbElement.getValue();
+							checkTable(tableElement);
+						}
 					}
 				}
 	}
@@ -469,10 +728,25 @@ public class LineCounter {
 	public static void checkChoice(Choice choice) {
 			List<JAXBElement<?>> choiceElementsList = choice.getAbbrOrExpanOrOrig();
 
-			for (JAXBElement<?> abbrOrExpanOrOrigElement : choiceElementsList) {
-				if (abbrOrExpanOrOrigElement.getValue() instanceof LbEtc) {
-					LbEtc lbEtc = (LbEtc)abbrOrExpanOrOrigElement.getValue();
+			for (JAXBElement<?> jaxbElement : choiceElementsList) {
+				if (jaxbElement.getValue() instanceof LbEtc) {
+					LbEtc lbEtc = (LbEtc)jaxbElement.getValue();
 					checkLbEtc(lbEtc);
+				}
+				else if(jaxbElement.getValue() instanceof Lb) 
+				{
+					Lb lbElement = (Lb) jaxbElement.getValue();
+					setNinLb(lbElement);
+				}
+				else if(jaxbElement.getValue() instanceof NameGND) 
+				{
+					NameGND nameGND = (NameGND) jaxbElement.getValue();
+					checkNameGnd(nameGND);
+				}
+				else if(jaxbElement.getValue() instanceof SourceGND) 
+				{
+					SourceGND sourceGND = (SourceGND) jaxbElement.getValue();
+					checkSourceGND(sourceGND);
 				}
 
 			}
@@ -491,15 +765,57 @@ public class LineCounter {
 					checkChoice(choice);
 
 				}
-			 else if (jaxbLbEtcListElement.getValue() instanceof Lb) {
-				
-				Lb LbElement = (Lb) jaxbLbEtcListElement.getValue();
-				setNinLb(LbElement);
-				
-			}
+			 	else if (jaxbLbEtcListElement.getValue() instanceof Lb) 
+				{				
+					Lb lbElement = (Lb) jaxbLbEtcListElement.getValue();
+					setNinLb(lbElement);				
+				}
+				else if (jaxbLbEtcListElement.getValue() instanceof NameGND) 
+				{				
+					NameGND nameGNDElement = (NameGND) jaxbLbEtcListElement.getValue();
+					checkNameGnd(nameGNDElement);			
+				}
+				else if (jaxbLbEtcListElement.getValue() instanceof SourceGND) 
+				{				
+					SourceGND sourceGNDElement = (SourceGND) jaxbLbEtcListElement.getValue();
+					checkSourceGND(sourceGNDElement);			
+				}
+				// else if (jaxbLbEtcListElement.getValue() instanceof Gap) 
+				// {				
+				// 	Gap gapElement = (Gap) jaxbLbEtcListElement.getValue();
+				// 	checkGap(gapElement);			
+				// }
+				// else if (jaxbLbEtcListElement.getValue() instanceof Num) 
+				// {				
+				// 	Num numElement = (Num) jaxbLbEtcListElement.getValue();
+				// 	checkNum(numElement);			
+				// }
+				else if (jaxbLbEtcListElement.getValue() instanceof Table) 
+				{				
+					Table tableElement = (Table) jaxbLbEtcListElement.getValue();
+					checkTable(tableElement);			
+				}
 		}
 		}
 	}
+
+	public static void checkFw(Fw fwElement) {
+
+		List<Serializable> fwElementList = fwElement.getContent();
+		for (Object fwElementListElement : fwElementList) {
+			if (!(fwElementListElement instanceof String)) {
+				JAXBElement<?> jaxbfwElementListElement = (JAXBElement<?>) fwElementListElement;
+			 	if (jaxbfwElementListElement.getValue() instanceof Lb) 
+				{				
+					Lb lbElement = (Lb) jaxbfwElementListElement.getValue();
+					setNinLb(lbElement);			
+				}
+				
+		}
+		}
+	}
+
+	
 
 	
 
