611f6490990d57723b9e46a816aa2af1945ac2b1
René Ackels <ackels@uni-trier.de>
2024-01-30 12:40:31 +0100

    structuring

--- tei_enrichment/src/main/java/de/uni_trier/bibliothek/xml/events/EventsMarshaller.java
+++ tei_enrichment/src/main/java/de/uni_trier/bibliothek/xml/events/EventsMarshaller.java
@@ -1,14 +1,14 @@
 // @author       René Ackels
 // Copyright (c) 2024 Universität Trier
 
-// This file is part of Keywording-Helper.
+// This file is part of TEI-Enrichment.
 
-// OCR-To-TEI is free software: you can redistribute it and/or modify
+// TEI-Enrichment is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Keywording-Helper is distributed in the hope that it will be useful,
+// TEI-Enrichment is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
